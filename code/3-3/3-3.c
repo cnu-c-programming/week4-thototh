51ef098a1ff040d7a4230da6c294ae5197c3a04d
thototh <144813512+thototh@users.noreply.github.com>
2026-03-27 12:30:38 +0900

    Update 3-3.c

---
+++ code/3-3/3-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void swap_endian(int *x){
+
+}
+
+int main(){
+    printf("sizeof(int*) = %zu\n", sizeof(int*));
+    printf("sizeof(double*) = %zu\n", sizeof(double*));
+    printf("sizeof(char*) = %zu\n", sizeof(char*));
+    printf("sizeof(void*) = %zu\n", sizeof(void*));
+
+    return 0;
+
+}
