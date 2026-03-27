501f408a79aab52711655509620c9cea5810c174
thototh <144813512+thototh@users.noreply.github.com>
2026-03-27 19:49:20 +0900

    Update 4-3.c

--- code/4-3/4-3.c
+++ code/4-3/4-3.c
@@ -10,8 +10,9 @@ int main(){
         //서로 인접한 두 요소의 실제 주소
         printf("address of arr[%d]: %p\n", i, addr_0);
         printf("address of arr[%d+1]: %p\n", i, addr_1);
+        //서로 인접한 두 요소의 주소 차이
         printf("&(arr[%d]) - &(arr[%d+1]) = %d\n", i , i+1, (unsigned long long)addr_1 - (unsigned long long)addr_0);
-        
+        printf("\n");
 
     }
     return 0;
