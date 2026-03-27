d462b0f79921c9f76b5fb3ac3356b484660cf8cd
thototh <144813512+thototh@users.noreply.github.com>
2026-03-27 11:58:25 +0900

    Update 2-3.c

---
+++ code/2-3/2-3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include <string.h>
+
+int main(){
+    char a[64] = "Hello";
+    char b[64] = "world";
+    char c[128];
+    
+    int len = strlen(a);
+    printf("length of a: %d\n", len);
+
+    strcpy(c, b);
+    printf("a: %s, c: %s\n", a, c);
+
+    strcpy(c, a);
+    strcat(c, " ");
+    strcat(c, b);
+    printf("c: %s\n", c);
+
+    printf("strcmp(%s, %s) = %d\n", a, a, strcmp(a, a));
+    printf("strcmp(%s, %s) = %d\n", a, b, strcmp(a, b));
+
+
+    return 0;
+}
